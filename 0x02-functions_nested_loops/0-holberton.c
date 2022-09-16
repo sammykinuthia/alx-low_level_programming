1d996445a8fe6719e22ee878ffb4484f0f48e654
sammykinuthia <samuelmwaniki17@gmail.com>
2022-09-15 19:43:41 -0700

    add final

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *main-program entry point for execution.
+ *Return:0-success
+ */
+int main(void)
+{
+	char i[] = "Holberton";
+	int j;
+
+	for (j = 0; j < 9; j++)
+	{
+		_putchar(i[j]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Contains all the exercises on functions on nested loops in c.
